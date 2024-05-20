9add672b850091b0d68a084246a952652be338d8
Lance Watanabe <phiferthree@gmail.com>
2024-05-19 22:37:29 -0700

    build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 // next.config.js
+const path = require("path");
+
 module.exports = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
+      // Configure Webpack to ignore test files
       config.module.rules.push({
         test: /\.test\.(js|jsx|ts|tsx)$/,
         loader: "ignore-loader",
