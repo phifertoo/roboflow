35a24e809bb438e697b6b422262f8e9ec8bdaf92
Lance Watanabe <phiferthree@gmail.com>
2024-05-20 00:55:48 -0700

    try again

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,13 @@
 // jest.config.js
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom", // Specify the test environment
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-    "^.+\\.(js|jsx)$": "babel-jest",
+    "^.+\\.(ts|tsx|js|jsx)?$": "babel-jest", // This line ensures Babel is used
   },
-  transformIgnorePatterns: ["node_modules/(?!@babel/runtime)"],
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
 };
